74cf6b73536cc10e32bd0ebe2715218b8c2ff35c
trent.wyatt <trent@trentwyatt.com>
2018-09-19 13:17:30 -0500

    Removed extra build step.  Changed to only keep last 3 builds

--- test.cpp
+++ test.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << std::endl << "Hello, world. This was automatically built by Jenkins!" << std::endl;
-    std::cout << "Changed the build to run the resulting program" << std::endl;
+    std::cout << std::endl << "Hello, world. This was automatically built by Jenkins!" << std::endl << std::endl;
     return 0;
 }
 
