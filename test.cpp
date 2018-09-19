89872de55a299ca4e4418b8d482ff3d577451651
trent.wyatt <trent@trentwyatt.com>
2018-09-19 13:01:25 -0500

    Added new build step to run program

--- test.cpp
+++ test.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << std::endl << "Hello, world. This was automatically built by Jenkins!" << std::endl << std::endl;
+    std::cout << std::endl << "Hello, world. This was automatically built by Jenkins!" << std::endl;
+    std::cout << "Changed the build to run the resulting program" << std::endl;
     return 0;
 }
 
